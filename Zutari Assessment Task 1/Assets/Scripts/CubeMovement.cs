79e437167c8e9beafe99f0c9460f3d0547970409
Liam Middleton <liammiddleton01@outlook.com>
2023-03-28 13:15:14 +0200

    Added Cube colour changes based on movement
    Using the WASD keys now chnages the colour of the cube based on its direction.

--- Zutari Assessment Task 1/Assets/Scripts/CubeMovement.cs
+++ Zutari Assessment Task 1/Assets/Scripts/CubeMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,20 +8,44 @@ public class CubeMovement : MonoBehaviour
     //Variables needed for movement
     public float speed = 5f;
     private Rigidbody rb;
+    private Renderer ren;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        ren = GetComponent<Renderer>();
+        
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
+        //Getting float variables for movement 
         float vertical = Input.GetAxis("Vertical");
         float horizontal = Input.GetAxis("Horizontal");
 
+        //Applying vector 3 force to move cube rigidbody
         Vector3 moveVect = new Vector3(horizontal, vertical, 0);
         rb.MovePosition(transform.position + moveVect * Time.deltaTime * speed);
+
+        if(Input.GetKeyDown(KeyCode.W))
+        {
+            ren.material.color = Color.blue;
+        }
+        else if(Input.GetKeyDown(KeyCode.S))
+        {
+            ren.material.color = Color.red;
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            ren.material.color = Color.green;
+        }
+        else if(Input.GetKeyDown(KeyCode.A)) 
+        {
+            ren.material.color = Color.yellow;
+        }
     }
+
 }
+
